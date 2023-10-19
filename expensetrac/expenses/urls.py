69d731e640a44c8163d6fc9e209cb3f2a005cce7
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-10-19 16:50:19 +0100

    initial setup for editing page

--- expensetrac/expenses/urls.py
+++ expensetrac/expenses/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('add_expenses/', views.add_expenses, name = 'add_expenses')
+    path('add_expenses/', views.add_expenses, name = 'add_expenses'),
+    path('edit_expenses/', views.edit_expenses, name = 'edit_expenses')
 ]
