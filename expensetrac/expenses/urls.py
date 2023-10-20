7a2abab97e816447a345ffb975373dacb9765fcc
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-10-20 13:10:51 +0100

    edit id added to path

--- expensetrac/expenses/urls.py
+++ expensetrac/expenses/urls.py
@@ -5,5 +5,5 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('add_expenses/', views.add_expenses, name = 'add_expenses'),
-    path('edit_expenses/', views.edit_expenses, name = 'edit_expenses')
+    path('edit_expenses/<int:id>', views.edit_expenses, name = 'edit_expenses')
 ]
