495d3df803fb81a2c0f125ea82cbff6dc7b9c791
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-10-30 12:18:55 +0100

    added export to csv urls n view

--- expensetrac/income/urls.py
+++ expensetrac/income/urls.py
@@ -4,14 +4,15 @@ from . import views
 from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
-    path('', views.index, name="index"),
-    path('add-income', views.add_income, name="add_income"),
-    path('edit-income/<int:id>', views.income_edit, name="income_edit"),
-    path('income-delete/<int:id>', views.delete_income, name="delete_income"),
-    path('search-income/', csrf_exempt(views.search_income),
-         name="search_income"),
-    path('income_source_summary', views.income_source_summary,
-         name="income_source_summary"),
-    path('stats', views.stats_view,
-         name="stats")
+     path('', views.index, name="index"),
+     path('add-income', views.add_income, name="add_income"),
+     path('edit-income/<int:id>', views.income_edit, name="income_edit"),
+     path('income-delete/<int:id>', views.delete_income, name="delete_income"),
+     path('search-income/', csrf_exempt(views.search_income),
+          name="search_income"),
+     path('income_source_summary', views.income_source_summary,
+          name="income_source_summary"),
+     path('stats', views.stats_view,
+          name="stats"),
+     path('export_csv', views.export_csv, name="export-csv"),
 ]
