31cd3bb1263d792b1b52416e3cedcf8fc14ea35d
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-10-30 12:38:48 +0100

    added export to excel urls n view

--- expensetrac/income/urls.py
+++ expensetrac/income/urls.py
@@ -15,4 +15,5 @@ urlpatterns = [
      path('stats', views.stats_view,
           name="stats"),
      path('export_csv', views.export_csv, name="export-csv"),
+     path('export_excel', views.export_excel, name="export-excel"),
 ]
