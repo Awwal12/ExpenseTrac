d677e3bcc993d6a9130dfbbbed14c2cfa41ea24c
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-11-02 10:52:35 +0100

    search url added

--- expensetrac/expenses/urls.py
+++ expensetrac/expenses/urls.py
@@ -9,10 +9,9 @@ urlpatterns = [
     path('add_expenses/', views.add_expenses, name = 'add_expenses'),
     path('edit_expenses/<int:id>', views.edit_expenses, name = 'edit_expenses'),
     path('delete_expenses/<int:id>', views.delete_expense, name = 'delete_expense'),
-    path('search-expenses/', csrf_exempt(views.search_expenses),
-        name="search-expenses"),
+    path('search/', views.search_expenses, name='search_expenses'),
     path('expense_category_summary', views.expense_category_summary,
-         name="expense_category_summary"),
+        name="expense_category_summary"),
     path('stats', views.stats_view,
-         name="stats")
+        name="stats")
 ]
