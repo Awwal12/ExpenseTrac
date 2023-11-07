37be422f603408dcfdad7166243c33c65c2e83b6
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-11-07 09:46:06 +0100

    update

--- expensetrac/expensetrac/settings.py
+++ expensetrac/expensetrac/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True if os.environ.get('DEBUG', 'False') == "True" else False
 
-ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST').spilt(" ")
+ALLOWED_HOSTS = os.environ.get('ALLOWED_HOST').split(" ")
 
 
 # Application definition
