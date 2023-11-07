0f9d2327a550a670d1343a6db45a93070f205f2c
Mohammed Moahmmed Awwal <abbaroze@gmail.com>
2023-11-07 10:32:42 +0100

    update

--- expensetrac/expensetrac/settings.py
+++ expensetrac/expensetrac/settings.py
@@ -154,7 +154,7 @@ STATIC_URL = 'static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "expensetrac/theme/static"),
+    os.path.join(BASE_DIR, "theme/static/"),
 ]
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
